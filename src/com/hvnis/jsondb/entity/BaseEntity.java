5c5e73003c5d61c146bf1eaed41e4bf581b6b6dc
Nis Hua Van <huavannis@gmail.com>
2018-11-08 06:02:18 +0100

    Add source

---
+++ src/com/hvnis/jsondb/entity/BaseEntity.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 Deveryware S.A. All Rights Reserved.
+ */
+package com.hvnis.jsondb.entity;
+
+/**
+ * Created by hvnis on 08/11/2018.
+ */
+public class BaseEntity {
+}
---
+++ src/com/hvnis/jsondb/repository/JsonRepository.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2018 Deveryware S.A. All Rights Reserved.
+ */
+package com.hvnis.jsondb.repository;
+
+/**
+ * Created by hvnis on 08/11/2018.
+ */
+public class JsonRepository {
+}
